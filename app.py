dec0c7343126b719cddd86522a8f3046cc273683
Abel <abel.baulloza@outlook.cl>
2023-06-11 16:49:41 -0400

    Routes actualizadas. Faltan las de sensor_data

--- app.py
+++ app.py
@@ -1,72 +1,100 @@
-from flask import Flask, jsonify,request
-from flask_cors import CORS
+from flask import Flask,request,jsonify
 from flask_login import login_required, LoginManager, login_user
-from werkzeug.security import generate_password_hash, check_password_hash
-
-from DB.database import get_db, create_tables
-from flask_sqlalchemy import SQLAlchemy
-
-import os
-from config import config
+import sqlite3
+from api.post import new_compania,new_location,new_sensor,sensordata
+from api.get import getall,getone
+from api.put import editlocation,editsensor
+from api.delete import deletelocation,deletesensor
 
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = ''
-db = SQLAlchemy(app)
-#db = get_db()
-#cursor = db.cursor()
-#create_tables(db,cursor)
 
-#login_manager_app=LoginManager(app)
+#CREAR COMPAÑIA CON USUARIO ADMIN
+@app.route('/api/v1/admin/new_compania', methods=['POST'])
+def init_admin_new_compania():
+   return new_compania()
 
-#INICIO API
-@app.route('/', methods=['GET'])
-def index():
-    print('Ingresar datos\n')
-    username = input("Username: ")
-    password = input("Password: ")
-    statement = "INSERT INTO ADMIN(username,password) VALUES (?,?)"
-    cursor.execute(statement, [username, password])
-    db.commit()
-    return 'Welcome to the API'
+#CREAR LUGAR CON USUARIO ADMIN
+@app.route('/api/v1/admin/new_location', methods=['POST'])
+def init_admin_new_location():
+   return new_location()
 
-#CREAR COMPAÑIA
-@app.route('/new_company')
+#CREAR SENSOR CON USUARIO ADMIN
+@app.route('/api/v1/admin/new_sensor', methods=['POST'])
+def init_admin_new_sensor():
+    return new_sensor()
 
-#LOGIN ADMIN
-@app.route('/login',methods=['POST'])
-def registro_admin():
-    cursor = db.cursor()
+#MOSTRAR TODO DE UNA TABLA
+@app.route('/api/getall/<tabla>&company_api_key=<company_api_key>', methods=['GET'])
+def init_getall(tabla,company_api_key):
+    return getall(tabla,company_api_key)
+
+#MOSTRAR UNO DE UNA TABLA
+@app.route('/api/getone/<tabla>&id=<id>&company_api_key=<company_api_key>', methods=['GET'])
+def init_getone(tabla, id, company_api_key):
+    return getone(tabla, id, company_api_key)
+
+#EDITAR UNO DE LA TABLA LOCATION
+@app.route('/api/edit/location/<id>', methods=['PUT'])
+def init_edit_location(id):
+    return editlocation(id)
+
+#EDITAR UNO DE LA TABLA SENSOR
+@app.route('/api/edit/sensor/<id>', methods=['PUT'])
+def init_edit_sensor(id):
+    return editsensor(id)
+
+#ELIMINAR UNO DE LA TABLA LOCATION
+@app.route('/api/delete/location/<id>&company_api_key=<company_api_key>', methods=['DELETE'])
+def init_delete_location(id,company_api_key):
+    return deletelocation(id,company_api_key)
+
+#ELIMINAR UNO DE LA TABLA SENSOR
+@app.route('/api/delete/sensor/<id>&company_api_key=<company_api_key>', methods=['DELETE'])
+def init_delete_sensor(id,company_api_key):
+    return deletesensor(id,company_api_key)
+
+#ENVIAR SENSOR_DATA
+@app.route('/api/v1/sensor_data/', methods=['POST'])
+def init_sensor_data():
+    return sensordata()
+
+
+
+@app.route('/login', methods=['POST'])
+def login():
     info = request.get_json()
     username = info['username']
     password = info['password']
-    password_hash = generate_password_hash(password)
-
-    if not username:
-        return jsonify({"message": "Se requiere username"})
-    if not password:
-        return jsonify({"message": "Se requiere password"})
-    
-    statement = "SELECT * FROM admin WHERE username = ? AND password = ?"
-    logeado = cursor.execute(statement, [username, password])
-    if logeado is not None:
-        print(logeado)
-        login_user(logeado)
-        return jsonify({"message": "Admin existente"})
-        
+    print(username)
+    print(password)
+    conn = sqlite3.connect('DB/database.db')
+    cursor = conn.cursor()
+    statement = "select * from admin where username = ?"
+    if cursor.execute(statement, [username]) is not None:
+        rows = cursor.fetchall()
+        print(rows)
+        return jsonify({"message":"Admin already exist"})
 
 
 
 
+#ENVÍO DE DATA DE SENSOR
+#@app.route('/api/v1/sensor_data', methods=['POST'])
+#def sensor_data():
+#    info = request.get_json()
+#    sensor_api_key = authorization_sensor(info['api_key'])
+#    if sensor_api_key == True:
+#        conn = sqlite3.connect('DB/database.db')
+#        cursor = conn.cursor()
+#        sensor_info = (info['json_data'][],info['json_data'][],info['json_data'][],info['json_data'][])
+#        query = """
+#               """
 
 
 
 
 
-
-
-
-
+#   return 'Hola'
 
 if __name__ == '__main__':
-    app.config.from_object(config['development'])
-    app.run()
+    app.run(debug=True)
